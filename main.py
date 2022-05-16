bf3ef54d6a2cc05211c81e47193ef46b4a51a43f
Deni <73035025+DeniabilityGreen@users.noreply.github.com>
2022-05-16 13:20:49 +0100

    python practice

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-mySprite: Sprite = None
+def on_on_overlap(sprite, otherSprite):
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
 scene.set_background_color(9)
 mySprite = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . a a a a a a a a . . . . .
-    . . . a a a a a a a a . . . . .
-    . a a a a a a a a a a a a . . .
-    . a a a a 7 a a 7 a a a a . . .
-    . a a a a a a a a a a a a . . .
-    . a a a a 7 a a 7 a a a a . . .
-    . a a a a 7 7 7 7 a a a a . . .
-    . . . a a a a a a a a . . . . .
-    . . . a a a a a a a a . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-""")
-, SpriteKind.player)
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . a a a a a a a a a a a a . . . 
+            . a a a a 7 a a 7 a a a a . . . 
+            . a a a a a a a a a a a a . . . 
+            . a a a a 7 a a 7 a a a a . . . 
+            . a a a a 7 7 7 7 a a a a . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
 mySprite.set_position(100, 100)
 controller.move_sprite(mySprite)
 pizza = sprites.create(img("""
-    . . . . . . b b b b . . . . . .
-    . . . . . . b 4 4 4 b . . . . .
-    . . . . . . b b 4 4 4 b . . . .
-    . . . . . b 4 b b b 4 4 b . . .
-    . . . . b d 5 5 5 4 b 4 4 b . .
-    . . . . b 3 2 3 5 5 4 e 4 4 b .
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e
-    . . . b 5 3 2 3 5 5 5 5 e e e e
-    . . b d 7 5 5 5 3 2 3 5 5 e e e
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 .
-    b d 3 2 d 5 5 5 d d d 4 4 . . .
-    b 5 5 5 5 d d 4 4 4 4 . . . . .
-    4 d d d 4 4 4 . . . . . . . . .
-    4 4 4 4 . . . . . . . . . . . .
-"""), SpriteKind.food)
-
-def on_overlap(sprite, otherSprite):
-    info.change_score_by(1)
-
+        . . . . . . b b b b . . . . . . 
+            . . . . . . b 4 4 4 b . . . . . 
+            . . . . . . b b 4 4 4 b . . . . 
+            . . . . . b 4 b b b 4 4 b . . . 
+            . . . . b d 5 5 5 4 b 4 4 b . . 
+            . . . . b 3 2 3 5 5 4 e 4 4 b . 
+            . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+            . . . b 5 3 2 3 5 5 5 5 e e e e 
+            . . b d 7 5 5 5 3 2 3 5 5 e e e 
+            . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+            . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+            . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+            b d 3 2 d 5 5 5 d d d 4 4 . . . 
+            b 5 5 5 5 d d 4 4 4 4 . . . . . 
+            4 d d d 4 4 4 . . . . . . . . . 
+            4 4 4 4 . . . . . . . . . . . .
+    """),
+    SpriteKind.food)
 pizza.set_position(randint(0, 160), randint(0, 120))
 info.start_countdown(10)
-sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_overlap)
--- main.ts
+++ main.ts
@@ -1,44 +1,44 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
     info.changeScoreBy(1)
 })
 scene.setBackgroundColor(9)
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . a a a a a a a a . . . . . 
-    . . . a a a a a a a a . . . . . 
-    . a a a a a a a a a a a a . . . 
-    . a a a a 7 a a 7 a a a a . . . 
-    . a a a a a a a a a a a a . . . 
-    . a a a a 7 a a 7 a a a a . . . 
-    . a a a a 7 7 7 7 a a a a . . . 
-    . . . a a a a a a a a . . . . . 
-    . . . a a a a a a a a . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . a a a a a a a a a a a a . . . 
+            . a a a a 7 a a 7 a a a a . . . 
+            . a a a a a a a a a a a a . . . 
+            . a a a a 7 a a 7 a a a a . . . 
+            . a a a a 7 7 7 7 a a a a . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . a a a a a a a a . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
     `, SpriteKind.Player)
 mySprite.setPosition(100, 100)
 controller.moveSprite(mySprite)
 let pizza = sprites.create(img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
+        . . . . . . b b b b . . . . . . 
+            . . . . . . b 4 4 4 b . . . . . 
+            . . . . . . b b 4 4 4 b . . . . 
+            . . . . . b 4 b b b 4 4 b . . . 
+            . . . . b d 5 5 5 4 b 4 4 b . . 
+            . . . . b 3 2 3 5 5 4 e 4 4 b . 
+            . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+            . . . b 5 3 2 3 5 5 5 5 e e e e 
+            . . b d 7 5 5 5 3 2 3 5 5 e e e 
+            . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+            . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+            . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+            b d 3 2 d 5 5 5 d d d 4 4 . . . 
+            b 5 5 5 5 d d 4 4 4 4 . . . . . 
+            4 d d d 4 4 4 . . . . . . . . . 
+            4 4 4 4 . . . . . . . . . . . .
     `, SpriteKind.Food)
 pizza.setPosition(randint(0, 160), randint(0, 120))
 info.startCountdown(10)
